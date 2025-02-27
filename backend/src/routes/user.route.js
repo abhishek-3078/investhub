24574639646c2b56195627e575fde8766e5d4a29
Abhishek <12213078@nitkkr.ac.in>
2025-02-27 17:26:55 +0530

    api for updating details

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -4,6 +4,7 @@ const router= require('express').Router();
 const Investor= require('../models/investor.model');
 const Startup= require('../models/startup.model');
 const authMiddleware= require('../middleware/auth.middleware');
+const { default: InvestorProfile } = require('../../../frontend/src/pages/InvestorProfile');
 
 router.get('/startup', authMiddleware, async(req, res) => {
     
@@ -16,5 +17,28 @@ router.get('/investor', authMiddleware, async(req, res) => {
   const data=await Investor.findById(req.user);
     res.status(200).json(data);
 });
+
+router.post('/startup', authMiddleware, async(req, res) => {
+    try{
+        
+        const startup= await Startup.findByIdAndUpdate(req
+            .user, req.body, {new:true});
+            res.status(200).json(startup);
+    }catch(error){
+        console.error("Error updating startup:", error);
+        res.status(500).json({message: "Server error", error: error.message});
+    }
+})
+router.post('/investor', authMiddleware, async(req, res) => {
+    try{
+        
+        const startup= await Investor.findByIdAndUpdate(req
+            .user, req.body, {new:true});
+            res.status(200).json(startup);
+    }catch(error){
+        console.error("Error updating startup:", error);
+        res.status(500).json({message: "Server error", error: error.message});
+    }
+})
 
 module.exports=router;
